eb65154be8812fec6e446758de13489d691edcb8
Gautam.A <gautam.abhyankar@gmail.com>
2020-05-14 03:09:25 +0530

    Change Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,8 +76,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome`}
+      description="The Bind Oriented Roblox-Discord Verification Bot">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
