93f84451016e3dfcefcd05a7bd455d1c43c779ea
Gautam.A <gautam.abhyankar@gmail.com>
2020-05-14 17:25:12 +0530

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,4 +55,7 @@ module.exports = {
       },
     ],
   ],
+  scripts: [
+    {src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", async: true, "data-ad-client": "ca-pub-1197269638005739"}
+  ]
 };
